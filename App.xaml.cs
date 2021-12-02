1b82ccb25beb55a4a01e647e637db6ab3acf27b8
ledengmig29 <50867425+ledengmig29@users.noreply.github.com>
2021-12-02 19:32:51 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ListViewData.View;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ListViewData
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+       // public ManagerSetting SettingManager = new ManagerSetting();
+
+        public static App CurrApp { get { return App.Current as App; } }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (new MainWindow().ShowDialog() ==true)
+            {
+                new MainWindow().ShowDialog();
+            }
+            Application.Current.Shutdown();
+        }
+    }
+}
